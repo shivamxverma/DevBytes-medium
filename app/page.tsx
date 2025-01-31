40e175f00058338996a30eaec8e8ba5ac3962b12
shivamxverma <shivam.verma.shiva.99@gmail.com>
2025-01-31 22:42:28 +0530

    created middleware

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import {useState} from "react";
 
 export default function Home() {
+  const [blogs , setblogs] = useState<string[]>([]);
+
+  
   return (
     <div>
       <h1>Hello i am in home page</h1>
+      <div><button>Add Blog</button></div>
+      
     </div>
   );
 }
---
+++ services/getDateFromToken.ts
@@ -0,0 +1,30 @@
+import { NextRequest } from "next/server";
+import jwt from 'jsonwebtoken';
+
+interface DecodedData {
+  id: string,
+  email: string,
+  name: string,
+  password: string,
+  posts : string[],
+  comments: string[],
+  followers: string[],
+  following: string[],
+  upvotes: string[],
+  createdAt: string,
+  updatedAt: string,
+}
+
+export function getDateFromToken(request: NextRequest) {
+  try{
+    const token = request.cookies.get('token')?.value || '';
+    const DecodedDataUser : DecodedData = jwt.verify(token, process.env.JWT_SECRET!) as DecodedData;
+
+    const id = DecodedDataUser.id;
+    return id;
+  } catch(err : any){
+    throw new Error(err.message);
+  }
+}
+
+
